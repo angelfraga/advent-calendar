566892659adb78d11d360ca2d1fc336736ea9054
angelfraga <angelfragap@outlook.es>
2020-12-20 14:57:37 +0100

    chore: webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.ts',
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            }
+		]
+	},
+	resolve: {
+		extensions: [".ts", ".js" ]
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+	devServer: {
+        hot: true,
+        inline: true,
+        contentBase:  '.',
+        compress: true
+    },
+    watchOptions: {
+        ignored: 'node_modules/**'
+    },
+	plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.html'
+		})
+	]
+};
