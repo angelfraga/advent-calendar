767f4277fa9d3ce1fdb7f9322cf929580de62d49
angelfraga <angelfragap@outlook.es>
2020-12-22 19:16:01 +0100

    chore(webpack): open browser on npm start

--- webpack.config.js
+++ webpack.config.js
@@ -4,50 +4,51 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.ts',
-	module: {
-		rules: [
-			{
-				test: /\.ts?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
             },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                  // Creates `style` nodes from JS strings
-                  "style-loader",
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
                 ],
             }
-		]
-	},
-	resolve: {
-		extensions: [".ts", ".js" ]
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map',
-	devServer: {
+    devServer: {
         hot: true,
         inline: true,
-        contentBase:  '.',
-        compress: true
+        contentBase: '.',
+        compress: true,
+        open: true
     },
     watchOptions: {
         ignored: 'node_modules/**'
     },
-	plugins: [
+    plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: 'src/index.html'
-		})
-	]
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ]
 };
